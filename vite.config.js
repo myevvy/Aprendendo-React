8a2ffef244db6e218a5e6eccd16e50c9d199713f
Evelyn Aquiles Silva de França <168788742+myevvy@users.noreply.github.com>
2026-06-18 00:45:59 +0000

    deploy 2

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react' // ou @vitejs/plugin-react
 
 export default defineConfig({
   plugins: [react()],
-  base: '/aprendendo-react/', 
+  base: '/Aprendendo-React/', 
 })
